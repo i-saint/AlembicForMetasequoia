48325aca29ad233d53d321df1189ec35b90946dd
i-saint <saint.skr@gmail.com>
2020-03-04 03:29:06 +0900

    mqabc wip

---
+++ mqabc/mqabc.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "MeshUtils/MeshUtils.h"
+#include "mqabc.h"
+
+mqabcAPI const char* mqabcGetPluginVersion()
+{
+    return mqabcVersionString;
+}
---
+++ mqabc/mqabc.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define mqabcVersion 0.1.0
+#define mqabcVersionString "0.1.0"
+
+#ifdef _WIN32
+    #define mqabcAPI extern "C" __declspec(dllexport)
+#else
+    #define mqabcAPI extern "C" 
+#endif
