cb59c917ae7acb77b594716f36270a306aa5a675
i-saint <saint.skr@gmail.com>
2020-03-04 03:02:43 +0900

    import MeshUtils

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo off
 
-cd External
+cd Externals
 
 IF NOT EXIST "7za.exe" (
     echo "downloading 7za.exe..."
@@ -14,14 +14,5 @@ powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[S
 echo "downloading mqsdk470.zip ..."
 powershell.exe -Command "wget http://www.metaseq.net/metaseq/mqsdk470.zip -OutFile mqsdk470.zip"
 7za.exe x -aos -o"mqsdk470" mqsdk470.zip
-
-echo "downloading mqsdk464.zip ..."
-powershell.exe -Command "wget http://www.metaseq.net/metaseq/mqsdk464.zip -OutFile mqsdk464.zip"
-7za.exe x -aos mqsdk464.zip
-
-echo "downloading mqsdk249c.zip ..."
-powershell.exe -Command "wget http://www.metaseq.net/metaseq/mqsdk249c.zip -OutFile mqsdk249c.zip"
-7za.exe x -aos -o"mqsdk249c" mqsdk249c.zip
-mklink /J mqsdk3 mqsdk249c\mqsdk
 
 cd ..
